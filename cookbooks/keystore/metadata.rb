3b82488b67a368bf6a28f39722dafe5b4b1ed99d
Vladimir Smirnov <vovan@vovans.com>
2012-10-31 14:23:34 -0700

    added java keystore management cookbook

---
+++ cookbooks/keystore/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "ZeddWorks"
+maintainer_email "scott.mcleod@zeddworks.com"
+license          "Apache 2.0"
+description      "Installs/Configures jks"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
