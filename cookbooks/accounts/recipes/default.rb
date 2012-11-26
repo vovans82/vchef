94f71ee4496019123edbf3d046d5617d17ee1215
Vladimir Smirnov <vovan@vovans.com>
2012-11-26 11:52:48 -0800

    modified accounts to include ruby-shadow package installation

--- cookbooks/accounts/recipes/default.rb
+++ cookbooks/accounts/recipes/default.rb
@@ -16,6 +16,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+package "ruby-shadow" do
+  action :install
+  not_if "rpm -q ruby-shadow"
+end
 
 include_recipe "accounts::users"
 include_recipe "accounts::groups"
